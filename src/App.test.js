cf27addfaa3df23584415f1c4e49e77e116b86f1
LeeYuseong <82145837+meteor-or@users.noreply.github.com>
2022-05-24 13:01:00 +0900

    feat: counter test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("카운터는 0부터 시작합니다.", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  //screen object를 이용해서 원하는 엘리먼트에 접근(접근할 때 ID로)
+  const counterElement = screen.getByTestId("counter")
+  //id가 counter인 엘리먼트의 텍스트가 0인지 테스트
+  expect(counterElement).toHaveTextContent(0);
+})
