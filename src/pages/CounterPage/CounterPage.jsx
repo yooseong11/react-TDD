1744e9779ed67105d157b23434f166f371754e84
LeeYuseong <82145837+meteor-or@users.noreply.github.com>
2022-05-27 13:22:40 +0900

    Fix: App -> CounterPage로 이동

---
+++ src/pages/CounterPage/CounterPage.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+const CounterPage = () => {
+	const [counter, setCounter] = useState(0);
+	const [disabled, setDisabled] = useState(false);
+
+	return (
+    <div>
+      <header className="App-header">
+        <h3 data-testid="counter">{counter}</h3>
+        <div>
+          <button
+            data-testid="minus-button"
+            onClick={() => {
+              setCounter((count) => count - 1);
+            }}
+            disabled={disabled}
+          >
+            -
+          </button>
+          <button
+            data-testid="plus-button"
+            onClick={() => {
+              setCounter((count) => count + 1);
+            }}
+            disabled={disabled}
+          >
+            +
+          </button>
+        </div>
+        <div>
+          <button
+            data-testid="on/off-button"
+            style={{ backgroundColor: "blue" }}
+            onClick={() => {
+              setDisabled((prev) => !prev);
+            }}
+          >
+            on/off
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default CounterPage
--- src/pages/CounterPage/tests/CounterPage.test.jsx
+++ src/pages/CounterPage/tests/CounterPage.test.jsx
@@ -1,28 +1,28 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import App from "./App";
+import CounterPage from "../CounterPage";
 
 test("카운터는 0부터 시작합니다.", () => {
-  render(<App />);
+  render(<CounterPage />);
   //screen object를 이용해서 원하는 엘리먼트에 접근(접근할 때 ID로)
-  const counterElement = screen.getByTestId("counter")
+  const counterElement = screen.getByTestId("counter");
   //id가 counter인 엘리먼트의 텍스트가 0인지 테스트
   expect(counterElement).toHaveTextContent(0);
-})
+});
 
-test("마이너스 버튼에 맞는 텍스트가 들어가있다.", ()=>{
-  render(<App />);
+test("마이너스 버튼에 맞는 텍스트가 들어가있다.", () => {
+  render(<CounterPage />);
   const minusButttonElement = screen.getByTestId("minus-button");
   expect(minusButttonElement).toHaveTextContent("-");
 });
 
 test("플러스 버튼에 맞는 텍스트가 들어가있다.", () => {
-  render(<App />);
+  render(<CounterPage />);
   const plusButttonElement = screen.getByTestId("plus-button");
   expect(plusButttonElement).toHaveTextContent("+");
 });
 
 test("플러스 버튼을 누르면 카운터가 1로 바뀝니다.", () => {
-  render(<App />);
+  render(<CounterPage />);
   const buttonElement = screen.getByTestId("plus-button");
   fireEvent.click(buttonElement);
   const counterElement = screen.getByTestId("counter");
@@ -30,7 +30,7 @@ test("플러스 버튼을 누르면 카운터가 1로 바뀝니다.", () => {
 });
 
 test("마이너스 버튼을 누르면 카운터가 -1로 바뀝니다.", () => {
-  render(<App />);
+  render(<CounterPage />);
   const buttonElement = screen.getByTestId("minus-button");
   fireEvent.click(buttonElement);
   const counterElement = screen.getByTestId("counter");
@@ -38,16 +38,16 @@ test("마이너스 버튼을 누르면 카운터가 -1로 바뀝니다.", () => 
 });
 
 test("on/off 버튼이 파란색 입니다.", () => {
-  render(<App />);
+  render(<CounterPage />);
   const buttonElement = screen.getByTestId("on/off-button");
   // getByTestId로 엘레멘트를 잡는 건 좋은 방법은 아니다.
-  expect(buttonElement).toHaveStyle({backgroundColor:"blue"});
-})
+  expect(buttonElement).toHaveStyle({ backgroundColor: "blue" });
+});
 
 test("on/off 버튼을 클릭하면 +, - 버튼이 눌리는 걸 막는다", () => {
-  render(<App />);
+  render(<CounterPage />);
   const onOffbuttonElement = screen.getByTestId("on/off-button");
   fireEvent.click(onOffbuttonElement);
   const plusButttonElement = screen.getByTestId("plus-button");
   expect(plusButttonElement).toBeDisabled();
-})
+});
