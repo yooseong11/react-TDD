c5cf6da128df49449824f7b7a0ba4a869f359e32
LeeYuseong <82145837+meteor-or@users.noreply.github.com>
2022-05-27 13:23:17 +0900

    Feat: Summary 페이지 Form 구현

---
+++ src/pages/SummaryPage/SummaryPage.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+const SummaryPage = () => {
+	const [checked, setChecked] = useState(false);
+
+	return (
+    <div>
+      <form>
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+          id="confirm-checkbox"
+        ></input>
+        <label htmlFor="confirm-checkbox">
+          주문하려는 것을 확인하셨나요?
+        </label>
+        <br />
+        <button disabled={!checked} type="submit">
+          주문확인
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default SummaryPage
