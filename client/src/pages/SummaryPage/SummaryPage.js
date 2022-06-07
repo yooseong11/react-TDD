bd1138534942824dfb1574ce48f16a8c16dd1321
LeeYuseong <82145837+meteor-or@users.noreply.github.com>
2022-06-07 18:01:21 +0900

    주문페이지 코드 작성

--- client/src/pages/SummaryPage/SummaryPage.js
+++ client/src/pages/SummaryPage/SummaryPage.js
@@ -1,11 +1,43 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import { OrderContext } from '../../contexts/OrderContext';
 
 const SummaryPage = ({ setStep }) => {
   const [checked, setChecked] = useState(false);
+  const [orderDatas] = useContext(OrderContext);
+
+  const productsArray = Array.from(orderDatas.products);
+  const productList = productsArray.map(([key, value]) => (
+    <li key={key}>
+      {value} {key}
+    </li>
+  ));
+
+  const hasOptions = orderDatas.options.size > 0;
+  let optionsRender = null;
+
+  if (hasOptions) {
+    const optionArray = Array.from(orderDatas.options.keys());
+    const optionList = optionArray.map((key) => <li key={key}>{key}</li>);
+    optionsRender = (
+      <>
+        <h2>옵션: {orderDatas.totals.options}</h2>
+        <ul>{optionList}</ul>
+      </>
+    );
+  }
+
+  const handleSubmit = (event) => {
+    event.prevent();
+    setStep(2);
+  }
 
   return (
     <div>
-      <form>
+      <h1>주문 확인</h1>
+      <h2>여행 상품: {orderDatas.totals.products}</h2>
+      <ul>{productList}</ul>
+      {optionsRender}
+      <form onSubmit={handleSubmit}>
         <input
           type="checkbox"
           checked={checked}
--- client/src/pages/SummaryPage/tests/SummaryPage.test.js
+++ client/src/pages/SummaryPage/tests/SummaryPage.test.js
@@ -1,5 +1,5 @@
 import SummaryPage from "../SummaryPage";
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../../../test-utils";
 
 test.only("checkbox and button", () => { 
 	render(<SummaryPage />);
