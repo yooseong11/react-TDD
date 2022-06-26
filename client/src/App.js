6825ae913cd0e0b70bf3a6b0a207406e8875e5e9
LeeYuseong <82145837+meteor-or@users.noreply.github.com>
2022-06-26 21:35:09 +0900

    Chore: delete App.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
-import OrderPage from "./pages/OrderPage"
+import OrderPage from "./pages/OrderPage";
 import SummaryPage from "./pages/SummaryPage";
 import CompletePage from "./pages/CompletePage";
-import './style/tailwind.css';
+import "./style/tailwind.css";
 import { OrderContextProvider } from "./contexts/OrderContext";
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
-const [step, setStep] = useState(0);
+  const [step, setStep] = useState(0);
 
   return (
     <div className="App">
