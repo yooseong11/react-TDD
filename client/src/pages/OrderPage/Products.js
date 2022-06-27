8042129635d9d00b337328cc510ad16bf99161ab
LeeYuseong <82145837+meteor-or@users.noreply.github.com>
2022-06-27 10:55:45 +0900

    Feat: + 버튼 클릭시 수량이 올라가고 가격이 업데이트

--- client/src/pages/OrderPage/Products.js
+++ client/src/pages/OrderPage/Products.js
@@ -1,35 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const Products = ({ name, imagePath, updateItemCount, price }) => {
-  const handleChange = (event) => {
-    const currentValue = event.target.value;
+    const [value, setValue] = useState(0);
 
-    updateItemCount(name, currentValue);
-  };
+    
+    const handleChange = (event) => {
+        const currentValue = event.target.value;
+        setValue(parseInt(currentValue));
+        updateItemCount(name, currentValue);
+    };
 
-  return (
-    <div className="mr-2 py-5">
-      <img
-        className="rounded w-full"
-        src={`${process.env.REACT_APP_API_URL}/${imagePath}`}
-        alt={`${name} product`}
-      />
-      <form className="flex flex-col p-1">
-        <label htmlFor={name}>{name}</label>
-        <p className="font-bold">{price}원</p>
-        <input
-          id={name}
-          className="font-semibold border p-2"
-          type="number"
-          name="quantity"
-          min={0}
-          defaultValue={0}
-          onChange={handleChange}
-        />
-        <button></button>
-      </form>
-    </div>
-  );
+    const onClickPlusBtn = function(e) {
+        updateItemCount(name, value + 1)
+        setValue((prev) => prev + 1);
+    };
+
+    return (
+        <div className="mr-2 py-5">
+            <img
+                className="rounded w-full"
+                src={`${process.env.REACT_APP_API_URL}/${imagePath}`}
+                alt={`${name} product`}
+            />
+            <div className="flex flex-col p-1">
+                <label htmlFor={name}>{name}</label>
+                <p className="font-bold">{price}원</p>
+                <input
+                    id={name}
+                    className="font-semibold border p-2"
+                    type="number"
+                    name="quantity"
+                    value={value}
+                    min={0}
+                    onChange={handleChange}
+                />
+                <button data-testid={`${name}PlusBtn`} onClick={onClickPlusBtn}>
+                    +
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Products;
